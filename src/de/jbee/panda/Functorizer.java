28a25925c1dee83c0517d3d7c1399ebae1410351
Jan Bernitt <jan.bernitt@gmx.de>
2011-10-03 21:42:04 +0200

    DOC updated Functorizer method description

--- src/de/jbee/panda/Functorizer.java
+++ src/de/jbee/panda/Functorizer.java
@@ -3,22 +3,18 @@ package de.jbee.panda;
 public interface Functorizer {
 
 	/**
-	 * Boxes the functor <code>f</code> into another behavioral functor identified by
-	 * <code>name</code>. It is used to resolve the {@linkplain Functor} bound to a new {@link Var}
-	 * inside a <code>let</code> expressions.
-	 * 
-	 * The <code>each</code> functor is an example.
-	 */
-	//Functor behaviour( String name, Functor f );
-
-	/**
 	 * Expands the <code>value</code> into a functor. The {@link Functor} chosen is resolved by the
 	 * value's type.
 	 */
 	Functor value( Object value );
 
 	/**
-	 * Boxes the value by a formatting functor named <code>name</code>.
+	 * Boxes the <code>value</code> into a behavioral functor identified by <code>name</code>.
+	 * 
+	 * This is used e.g. to resolve the {@linkplain Functor} bound to a new {@link Var} inside a
+	 * <code>let</code> expressions. The <code>each</code> functor is an example.
+	 * 
+	 * Note that the value might already be a {@linkplain Functor}. This doesn't make a difference.
 	 */
 	Functor behaviour( String name, Object value );
 
